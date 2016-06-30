d10bc57c2a2abb04fd623e736562278294e113d8
Randy Scroggins <scroggins@csus.edu>
2016-06-30 16:56:31 -0700

    more gui stuff

---
+++ WeMakeGameNow/SpellsGui.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeMakeGameNow
+{
+    public partial class SpellsGui : Form
+    {
+        public SpellsGui()
+        {
+            InitializeComponent();
+        }
+    }
+}
