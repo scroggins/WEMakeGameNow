e70dfc2e790a0e3cb6c30fb9d3af57cc3232d531
Randy Scroggins <scroggins@csus.edu>
2016-07-01 21:05:08 -0700

    made game options Gui with Duke

--- WeMakeGameNow/GameOptionsGui.Designer.cs
+++ WeMakeGameNow/GameOptionsGui.Designer.cs
@@ -28,20 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.returnToTitleButton = new System.Windows.Forms.Button();
+            this.backToGameButton = new System.Windows.Forms.Button();
+            this.craftingButton = new System.Windows.Forms.Button();
+            this.gameOptionsButton = new System.Windows.Forms.Button();
+            this.spellsButton = new System.Windows.Forms.Button();
+            this.inventoryButton = new System.Windows.Forms.Button();
+            this.characterButton = new System.Windows.Forms.Button();
+            this.soundPanel = new System.Windows.Forms.Panel();
+            this.controlPanel = new System.Windows.Forms.Panel();
+            this.interfaceVideoPanel = new System.Windows.Forms.Panel();
+            this.soundLable = new System.Windows.Forms.Label();
+            this.controlsLabel = new System.Windows.Forms.Label();
+            this.interfaceVideoLabel = new System.Windows.Forms.Label();
+            this.soundPanel.SuspendLayout();
+            this.controlPanel.SuspendLayout();
+            this.interfaceVideoPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // returnToTitleButton
+            // 
+            this.returnToTitleButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.returnToTitleButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.returnToTitleButton.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.returnToTitleButton.Location = new System.Drawing.Point(12, 636);
+            this.returnToTitleButton.Name = "returnToTitleButton";
+            this.returnToTitleButton.Size = new System.Drawing.Size(157, 98);
+            this.returnToTitleButton.TabIndex = 28;
+            this.returnToTitleButton.Text = "Return To Title";
+            this.returnToTitleButton.UseVisualStyleBackColor = false;
+            this.returnToTitleButton.Click += new System.EventHandler(this.returnToTitleButton_Click);
+            // 
+            // backToGameButton
+            // 
+            this.backToGameButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.backToGameButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.backToGameButton.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backToGameButton.Location = new System.Drawing.Point(12, 532);
+            this.backToGameButton.Name = "backToGameButton";
+            this.backToGameButton.Size = new System.Drawing.Size(157, 98);
+            this.backToGameButton.TabIndex = 27;
+            this.backToGameButton.Text = "Back To Game (ESC)";
+            this.backToGameButton.UseVisualStyleBackColor = false;
+            this.backToGameButton.Click += new System.EventHandler(this.backToGameButton_Click);
+            // 
+            // craftingButton
+            // 
+            this.craftingButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.craftingButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.craftingButton.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.craftingButton.Location = new System.Drawing.Point(12, 324);
+            this.craftingButton.Name = "craftingButton";
+            this.craftingButton.Size = new System.Drawing.Size(157, 98);
+            this.craftingButton.TabIndex = 26;
+            this.craftingButton.Text = "Crafting";
+            this.craftingButton.UseVisualStyleBackColor = false;
+            this.craftingButton.Click += new System.EventHandler(this.craftingButton_Click);
+            // 
+            // gameOptionsButton
+            // 
+            this.gameOptionsButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gameOptionsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.gameOptionsButton.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameOptionsButton.Location = new System.Drawing.Point(12, 428);
+            this.gameOptionsButton.Name = "gameOptionsButton";
+            this.gameOptionsButton.Size = new System.Drawing.Size(157, 98);
+            this.gameOptionsButton.TabIndex = 25;
+            this.gameOptionsButton.Text = "Game Options";
+            this.gameOptionsButton.UseVisualStyleBackColor = false;
+            this.gameOptionsButton.Click += new System.EventHandler(this.gameOptionsButton_Click);
+            // 
+            // spellsButton
+            // 
+            this.spellsButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.spellsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.spellsButton.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spellsButton.Location = new System.Drawing.Point(12, 220);
+            this.spellsButton.Name = "spellsButton";
+            this.spellsButton.Size = new System.Drawing.Size(157, 98);
+            this.spellsButton.TabIndex = 24;
+            this.spellsButton.Text = "Spells";
+            this.spellsButton.UseVisualStyleBackColor = false;
+            this.spellsButton.Click += new System.EventHandler(this.spellsButton_Click);
+            // 
+            // inventoryButton
+            // 
+            this.inventoryButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.inventoryButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.inventoryButton.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inventoryButton.Location = new System.Drawing.Point(12, 116);
+            this.inventoryButton.Name = "inventoryButton";
+            this.inventoryButton.Size = new System.Drawing.Size(157, 98);
+            this.inventoryButton.TabIndex = 23;
+            this.inventoryButton.Text = "Inventory";
+            this.inventoryButton.UseVisualStyleBackColor = false;
+            this.inventoryButton.Click += new System.EventHandler(this.inventoryButton_Click);
+            // 
+            // characterButton
+            // 
+            this.characterButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.characterButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.characterButton.Font = new System.Drawing.Font("Lucida Sans Unicode", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.characterButton.Location = new System.Drawing.Point(12, 12);
+            this.characterButton.Name = "characterButton";
+            this.characterButton.Size = new System.Drawing.Size(157, 98);
+            this.characterButton.TabIndex = 22;
+            this.characterButton.TabStop = false;
+            this.characterButton.Text = "Character Info";
+            this.characterButton.UseVisualStyleBackColor = false;
+            this.characterButton.Click += new System.EventHandler(this.characterButton_Click);
+            // 
+            // soundPanel
+            // 
+            this.soundPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.soundPanel.Controls.Add(this.soundLable);
+            this.soundPanel.Location = new System.Drawing.Point(195, 12);
+            this.soundPanel.Name = "soundPanel";
+            this.soundPanel.Size = new System.Drawing.Size(859, 246);
+            this.soundPanel.TabIndex = 29;
+            this.soundPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.soundPanel_Paint);
+            // 
+            // controlPanel
+            // 
+            this.controlPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.controlPanel.Controls.Add(this.controlsLabel);
+            this.controlPanel.Location = new System.Drawing.Point(195, 264);
+            this.controlPanel.Name = "controlPanel";
+            this.controlPanel.Size = new System.Drawing.Size(859, 262);
+            this.controlPanel.TabIndex = 30;
+            this.controlPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.controlPanel_Paint);
+            // 
+            // interfaceVideoPanel
+            // 
+            this.interfaceVideoPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.interfaceVideoPanel.Controls.Add(this.interfaceVideoLabel);
+            this.interfaceVideoPanel.Location = new System.Drawing.Point(195, 536);
+            this.interfaceVideoPanel.Name = "interfaceVideoPanel";
+            this.interfaceVideoPanel.Size = new System.Drawing.Size(859, 246);
+            this.interfaceVideoPanel.TabIndex = 31;
+            this.interfaceVideoPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.interfaceVideoPanel_Paint);
+            // 
+            // soundLable
+            // 
+            this.soundLable.AutoSize = true;
+            this.soundLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.soundLable.Location = new System.Drawing.Point(21, 17);
+            this.soundLable.Name = "soundLable";
+            this.soundLable.Size = new System.Drawing.Size(74, 25);
+            this.soundLable.TabIndex = 0;
+            this.soundLable.Text = "Sound";
+            this.soundLable.Click += new System.EventHandler(this.soundLable_Click);
+            // 
+            // controlsLabel
+            // 
+            this.controlsLabel.AutoSize = true;
+            this.controlsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.controlsLabel.Location = new System.Drawing.Point(21, 19);
+            this.controlsLabel.Name = "controlsLabel";
+            this.controlsLabel.Size = new System.Drawing.Size(92, 25);
+            this.controlsLabel.TabIndex = 1;
+            this.controlsLabel.Text = "Controls";
+            this.controlsLabel.Click += new System.EventHandler(this.controlsLabel_Click);
+            // 
+            // interfaceVideoLabel
+            // 
+            this.interfaceVideoLabel.AutoSize = true;
+            this.interfaceVideoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.interfaceVideoLabel.Location = new System.Drawing.Point(21, 11);
+            this.interfaceVideoLabel.Name = "interfaceVideoLabel";
+            this.interfaceVideoLabel.Size = new System.Drawing.Size(175, 25);
+            this.interfaceVideoLabel.TabIndex = 2;
+            this.interfaceVideoLabel.Text = "InterFace / Video";
+            this.interfaceVideoLabel.Click += new System.EventHandler(this.interfaceVideoLabel_Click);
             // 
             // GameOptionsGui
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1066, 794);
+            this.Controls.Add(this.interfaceVideoPanel);
+            this.Controls.Add(this.controlPanel);
+            this.Controls.Add(this.soundPanel);
+            this.Controls.Add(this.returnToTitleButton);
+            this.Controls.Add(this.backToGameButton);
+            this.Controls.Add(this.craftingButton);
+            this.Controls.Add(this.gameOptionsButton);
+            this.Controls.Add(this.spellsButton);
+            this.Controls.Add(this.inventoryButton);
+            this.Controls.Add(this.characterButton);
             this.Name = "GameOptionsGui";
             this.Text = "GameOptionsGui";
             this.Load += new System.EventHandler(this.GameOptionsGui_Load);
+            this.soundPanel.ResumeLayout(false);
+            this.soundPanel.PerformLayout();
+            this.controlPanel.ResumeLayout(false);
+            this.controlPanel.PerformLayout();
+            this.interfaceVideoPanel.ResumeLayout(false);
+            this.interfaceVideoPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button returnToTitleButton;
+        private System.Windows.Forms.Button backToGameButton;
+        private System.Windows.Forms.Button craftingButton;
+        private System.Windows.Forms.Button gameOptionsButton;
+        private System.Windows.Forms.Button spellsButton;
+        private System.Windows.Forms.Button inventoryButton;
+        private System.Windows.Forms.Button characterButton;
+        private System.Windows.Forms.Panel soundPanel;
+        private System.Windows.Forms.Label soundLable;
+        private System.Windows.Forms.Panel controlPanel;
+        private System.Windows.Forms.Panel interfaceVideoPanel;
+        private System.Windows.Forms.Label controlsLabel;
+        private System.Windows.Forms.Label interfaceVideoLabel;
     }
 }
--- WeMakeGameNow/GameOptionsGui.cs
+++ WeMakeGameNow/GameOptionsGui.cs
@@ -21,5 +21,86 @@ namespace WeMakeGameNow
         {
 
         }
+
+        private void characterButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            CharacterInfoGui charInfoGui = new CharacterInfoGui();
+            charInfoGui.Show();
+            this.Close();
+        }
+
+        private void inventoryButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            InventoryGui inventoryGui = new InventoryGui();
+            inventoryGui.Show();
+            this.Close();
+        }
+
+        private void spellsButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            SpellsGui spellGui = new SpellsGui();
+            spellGui.Show();
+            this.Close();
+        }
+
+        private void craftingButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            CraftingGui craftingGui = new CraftingGui();
+            craftingGui.Show();
+            this.Close();
+        }
+
+        private void gameOptionsButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            OptionMenuGui optionMenuGui = new OptionMenuGui();
+            optionMenuGui.Show();
+            this.Close();
+        }
+
+        private void backToGameButton_Click(object sender, EventArgs e)
+        {
+            //need to return to game and close this gui 
+            this.Close();
+        }
+
+        private void returnToTitleButton_Click(object sender, EventArgs e)
+        {
+            //need a title to return to
+        }
+
+        private void soundLable_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void soundPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void controlsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void controlPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void interfaceVideoLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void interfaceVideoPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
